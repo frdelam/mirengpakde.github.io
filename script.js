3a70db5dda2540089341015b4ce32dfb9aa44c74
frdelam <125348784+frdelam@users.noreply.github.com>
2023-02-14 21:13:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("helo bubub, r u ready?");
+
+let pindah=confirm("wanna move?if u tap OK dont forget to comeback onemore time and tap cancel");
+if(pindah == true){
+window.location = "https://t.me/+AbnP-15BQIc2Yzc1/";}
+else{document.write("");
+}
